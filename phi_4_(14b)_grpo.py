a465e256c45f5efa295a75dacd9b402e16127498
JiangYingjin <i@JYJ.cx>
2025-04-26 20:27:44 +0800

    OOM 问题没有解决

--- phi_4_(14b)_grpo.py
+++ phi_4_(14b)_grpo.py
@@ -33,29 +33,45 @@ Load up `Phi-4 14B`, and set parameters
 from unsloth import FastLanguageModel, is_bfloat16_supported
 import torch
 
-max_seq_length = 512  # Can increase for longer reasoning traces
+max_seq_length = 1024  # Can increase for longer reasoning traces
+# max_seq_length = 2048  # Can increase for longer reasoning traces
+# max_seq_length = 512  # Can increase for longer reasoning traces
 lora_rank = 16  # Larger rank = smarter, but slower
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name="unsloth/Phi-4",
-    max_seq_length=max_seq_length,
+    max_seq_length=max_seq_length,  # 也许这个选项可以提升响应长度
     load_in_4bit=True,  # False for LoRA 16bit
     fast_inference=True,  # Enable vLLM fast inference
     max_lora_rank=lora_rank,
     gpu_memory_utilization=0.7,  # Reduce if out of memory
 )
 
+"""
+超参数有数百万种可能的搭配，选择正确的值对于微调至关重要。你将学习超参数的最佳实践——基于数百篇研究论文/实验的洞见及其对模型的影响。 我们建议你使用 Unsloth 预设的默认值。
+"""
+
 model = FastLanguageModel.get_peft_model(
     model,
-    r=lora_rank,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    r=lora_rank,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128。微调过程的秩。数值越大，占用的内存越多，速度越慢，但可以提高复杂任务的准确率。我们通常建议使用 8（用于快速微调），最高可达 128。数值过大可能会导致过拟合，从而损害模型质量。
+    # target_modules=[
+    #     "gate_proj",
+    #     "up_proj",
+    #     "down_proj",
+    # ], # 原 nb 设置
     target_modules=[
+        # "q_proj",
+        # "k_proj",
+        # "v_proj",
+        # "o_proj",
         "gate_proj",
         "up_proj",
         "down_proj",
-    ],
-    lora_alpha=lora_rank,
-    use_gradient_checkpointing="unsloth",  # Enable long context finetuning
-    random_state=3407,
+    ],  # 我们选择所有模块进行微调。您可以移除一些模块以减少内存占用并加快训练速度，但我们强烈建议您不要这样做。只需在所有模块上进行训练即可！
+    lora_alpha=lora_rank,  # 微调的缩放因子。较大的数值将使微调更深入地了解您的数据集，但可能会导致过拟合。我们建议将其设置为等于秩 r 或其两倍。
+    use_gradient_checkpointing="unsloth",  # Enable long context finetuning。选项包括 True 、 False 和 "unsloth" 。我们建议使用 "unsloth" ，因为它可以额外减少 30% 的内存使用量，并且支持超长的上下文微调。
+    random_state=3407,  # 随机数 seed，可使实验具有可重复性
+    max_seq_length=max_seq_length,  # 最大上下文长度。我们建议使用 2048 或 3072
 )
 
 """### Data Prep
@@ -185,7 +201,8 @@ from trl import GRPOConfig, GRPOTrainer
 
 training_args = GRPOConfig(
     use_vllm=True,  # use vLLM for fast inference!
-    learning_rate=5e-6,
+    learning_rate=2e-5,  # 典型范围 ：1e-4（0.0001）到 5e-5（0.00005），较低的值可实现较慢但更精确的微调。尝试使用 1e-4 、 5e-5 或 2e-5 等值。
+    # learning_rate=5e-6,  # 典型范围 ：1e-4（0.0001）到 5e-5（0.00005），较低的值可实现较慢但更精确的微调。尝试使用 1e-4 、 5e-5 或 2e-5 等值。
     adam_beta1=0.9,
     adam_beta2=0.99,
     weight_decay=0.1,
@@ -195,15 +212,20 @@ training_args = GRPOConfig(
     logging_steps=1,
     bf16=is_bfloat16_supported(),
     fp16=not is_bfloat16_supported(),
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=1,  # Increase to 4 for smoother training
+    # per_device_train_batch_size=2,  # 默认值为2，增加该值可提高 GPU 利用率，但要注意填充会导致训练速度变慢。为了更顺畅地训练，建议增加 gradient_accumulation_steps 。
+    per_device_train_batch_size=1,  # 增加该值可提高 GPU 利用率，但要注意填充会导致训练速度变慢。为了更顺畅地训练，建议增加 gradient_accumulation_steps 。
+    # gradient_accumulation_steps=4,  # 默认值为4，Increase to 4 for smoother training，模拟更大的批次大小而不增加内存使用量
+    gradient_accumulation_steps=1,  # Increase to 4 for smoother training，模拟更大的批次大小而不增加内存使用量
     num_generations=2,  # Decrease if out of memory
     # num_generations=6,  # Decrease if out of memory
-    max_prompt_length=256,
+    max_prompt_length=512,
+    # max_prompt_length=256,
     # max_completion_length=200,
-    max_completion_length=3000,
-    # num_train_epochs = 1, # Set to 1 for a full training run
-    max_steps=100,
+    # max_completion_length=3072,
+    max_completion_length=max_seq_length,
+    # num_train_epochs=1,  # Set to 1 for a full training run，一般设置为1-3，更多的 epoch 学习会导致过拟合，较少的 epoch 学习会导致训练不足。
+    # 完全跑完一个 epoch 需要迭代 1868 次，120个小时（5天时间）
+    max_steps=100,  # 加快训练速度。对于完整运行，请替换为 num_train_epochs = 1 （建议进行 1-3 次迭代以避免过拟合）。
     save_steps=250,
     max_grad_norm=0.1,
     report_to="none",  # Can use Weights & Biases
