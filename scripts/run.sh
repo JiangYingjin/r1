f54241d5abdc63e0d9266a2ddd7ef13e7303ba63
JiangYingjin <i@JYJ.cx>
2025-04-27 18:12:16 +0800

    进行了几次百步训练

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,9 @@
 export HF_HUB_ENABLE_HF_TRANSFER=0
 export VLLM_USE_V1=0
 
-python qwen2_5_\(3b\)_grpo.py | tee logs/$(date +"%d_%H%M").log
+export CUDA_VISIBLE_DEVICES=0
+
+# script_name=qwen2_5_\(3b\)_grpo.py
+script_name=t1.py
+
+python $script_name | tee logs/$(date +"%d_%H%M").log
