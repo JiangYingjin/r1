0c23a3f778c7f8e50a7d2bffc34b123c7755b4d4
JiangYingjin <i@JYJ.cx>
2025-05-12 15:05:50 +0800

    调整 eval 脚本的组织

--- scripts/eval/qwen2.5_1.5b.sh
+++ scripts/eval/qwen2.5_1.5b.sh
@@ -6,12 +6,13 @@
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 140
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 150
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 160
-python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 180
+# python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 180
 
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 190
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 210
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 240
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 250
 # python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 260
-# python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 290
-# python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 300
+python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 280
+python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 290
+python eval.py --model_name "unsloth/qwen2.5-1.5b-instruct-bnb-4bit" --exp_name "better_reward_qwen2.5_1.5b" --step 300
