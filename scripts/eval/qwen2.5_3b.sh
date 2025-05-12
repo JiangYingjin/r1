494cd10ef22cce0b064f74e558277ed6eceab52a
JiangYingjin <i@JYJ.cx>
2025-05-12 21:25:53 +0800

    评估长响应输出

--- scripts/eval/qwen2.5_3b.sh
+++ scripts/eval/qwen2.5_3b.sh
@@ -1,4 +1,7 @@
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 100
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 190
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 210
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 290
+# # python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 100
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 190
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 210
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 290
+
+python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 100
+python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 300
