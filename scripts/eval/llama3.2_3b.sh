b6b428906b87caa1660ff08eb4eef97098d16cd3
JiangYingjin <i@JYJ.cx>
2025-05-12 19:34:26 +0800

    扩大评估 llama

--- scripts/eval/llama3.2_3b.sh
+++ scripts/eval/llama3.2_3b.sh
@@ -1,8 +1,11 @@
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 50
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 150
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 200
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 90
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --no_chat_template
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" 
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 300
-# python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 250
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --no_chat_template
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" 
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" 
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 60
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 140
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 160
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 190
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 210
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 240
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 260
+python eval.py --model_name "unsloth/llama-3.2-3b-instruct-bnb-4bit" --exp_name "better_reward_llama" --step 290
