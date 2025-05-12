1af39798e81117598e2a8a0ef2bb3f8da5a46c61
JiangYingjin <i@JYJ.cx>
2025-05-12 17:01:15 +0800

    评估 Phi

--- scripts/eval/phi3.5_mini.sh
+++ scripts/eval/phi3.5_mini.sh
@@ -1,4 +1,6 @@
-python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit" --no_chat_template
 python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit"
+python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit"
+python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit" --no_chat_template
+python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit" --no_chat_template
 python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit" --exp_name "better_reward_phi3.5" --step 200
 python eval.py --model_name "unsloth/Phi-3.5-mini-instruct-bnb-4bit" --exp_name "better_reward_phi3.5" --step 100
