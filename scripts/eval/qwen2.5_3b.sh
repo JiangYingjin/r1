80360297274c1299a18292e9f8bddb517ba6f096
JiangYingjin <i@JYJ.cx>
2025-05-12 23:06:48 +0800

    更新评估脚本

--- scripts/eval/qwen2.5_3b.sh
+++ scripts/eval/qwen2.5_3b.sh
@@ -3,7 +3,7 @@
 # python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 210
 # python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "gsmplus600_course_1" --step 290
 
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 50 --debug
+python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 50
 # python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 100
 python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 150
 python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "course_2" --step 190
