cf11ed23c8bd703bf2382c0d61bd5c72deaa74db
JiangYingjin <i@JYJ.cx>
2025-05-13 11:27:43 +0800

    继续评估 Qwen

--- scripts/eval/qwen2.5_3b_20250427_181546.sh
+++ scripts/eval/qwen2.5_3b_20250427_181546.sh
@@ -1,12 +1,13 @@
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 7000
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 4000
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 2000
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 1000
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 7000
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 4000
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 2000
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 1000
 
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 100
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 300
-python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 200
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 100
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 300
+# python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 200
 
+python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 4100
 python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 6000
 python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 3000
 python eval.py --model_name "unsloth/Qwen2.5-3B-Instruct-unsloth-bnb-4bit" --exp_name "20250427_181546" --step 5000
